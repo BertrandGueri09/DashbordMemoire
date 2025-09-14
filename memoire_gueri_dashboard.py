c64b5b599c9d8a33a4bc76e674de50e029252479
NGAGABA Bertrand Gueri <bertrandgueri@gmail.com>
2025-09-14 23:07:45 +0000

    Update memoire_gueri_dashboard.py

--- memoire_gueri_dashboard.py
+++ memoire_gueri_dashboard.py
@@ -880,10 +880,12 @@ def main():
     eq_fig = go.Figure()
     eq_fig.add_trace(go.Scatter(x=bt_df['Date'], y=bt_df['equity'], mode='lines', name='Équity'))
     eq_fig.update_layout(height=280, margin=dict(t=6,b=6,l=6,r=6))
+    set_fig_template(eq_fig)
     st.plotly_chart(eq_fig, use_container_width=True, config={"displaylogo": False})
     
 if __name__ == "__main__":
     main()
+
 
 
 
